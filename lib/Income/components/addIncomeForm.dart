d1aab830c2aaa04be361e2351b0ce5796b807fc1
Christian Santiago Berio <c.santiagoberio@gmail.com>
2021-08-23 20:32:00 -0400

    Started logic on AddIncome form fields

--- lib/Income/components/addIncomeForm.dart
+++ lib/Income/components/addIncomeForm.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:intl/intl.dart';
 
 import '../../constants.dart';
 
@@ -13,14 +14,18 @@ class AddIncomeForm extends StatefulWidget {
 class _AddIncomeFormState extends State<AddIncomeForm> {
   late FocusNode _fNCateogry;
   late FocusNode _fnQuantity;
-  late FocusNode _fnDate;
+  late FocusNode _fnDateTime;
   late FocusNode _fNDescription;
 
   final _formKey = GlobalKey<FormState>();
 
+  final _dateTimeController = TextEditingController();
+
   int? _category = 0;
   int? _quantity = 0;
-  DateTime? _date = DateTime.now();
+  DateTime _date = DateTime.now();
+  TimeOfDay _time = TimeOfDay.now();
+  DateTime _dateTime = new DateTime.now();
   String? _description = "";
 
   @override
@@ -28,16 +33,19 @@ class _AddIncomeFormState extends State<AddIncomeForm> {
     super.initState();
     _fNCateogry = FocusNode();
     _fnQuantity = FocusNode();
-    _fnDate = FocusNode();
+    _fnDateTime = FocusNode();
     _fNDescription = FocusNode();
+
+    _dateTimeController.text = formatDate(_dateTime);
   }
 
   @override
   void dispose() {
     _fNCateogry.dispose();
     _fnQuantity.dispose();
-    _fnDate.dispose();
+    _fnDateTime.dispose();
     _fNDescription.dispose();
+    _dateTimeController.dispose();
     super.dispose();
   }
 
@@ -125,16 +133,16 @@ class _AddIncomeFormState extends State<AddIncomeForm> {
             Padding(
               padding: const EdgeInsets.symmetric(horizontal: 25, vertical: 5),
               child: TextFormField(
-                focusNode: _fnQuantity,
+                focusNode: _fnDateTime,
+                controller: _dateTimeController,
                 keyboardType: TextInputType.datetime,
-                initialValue: "",
-                onTap: () {},
+                onTap: () {
+                  _selectDateTime();
+                },
                 onSaved: (value) {
+                  print(value);
                   if (value != null) {
-                    final int? intValue = int.tryParse(value);
-                    if (intValue != null) {
-                      _quantity = intValue;
-                    }
+                    print(value);
                   }
                 },
                 validator: (value) {
@@ -153,10 +161,10 @@ class _AddIncomeFormState extends State<AddIncomeForm> {
                   alignLabelWithHint: true,
                   filled: true,
                   fillColor: Colors.white,
-                  hintText: "0.00",
-                  hintStyle: TextStyle(fontSize: 24),
+                  hintText: "MM/DD/YYYY HH:MM AM/PM",
+                  hintStyle: TextStyle(fontSize: 18),
                   prefixIcon: Icon(
-                    Icons.attach_money,
+                    Icons.calendar_today,
                     color:
                         _fnQuantity.hasFocus ? kippyDarkGreenOne : Colors.grey,
                     size: 24,
@@ -181,5 +189,66 @@ class _AddIncomeFormState extends State<AddIncomeForm> {
             )
           ],
         ));
+  }
+
+  Future _selectDateTime() async {
+    final date = await _selectDate();
+    if (date == null) {
+      return;
+    }
+
+    final time = await _selectTime();
+    if (time == null) return;
+
+    final dateTime = DateTime(
+      date.year,
+      date.month,
+      date.day,
+      time.hour,
+      time.minute,
+    );
+
+    _dateTimeController.text = formatDate(dateTime);
+
+    setState(() {
+      _dateTime = DateTime(
+        date.year,
+        date.month,
+        date.day,
+        time.hour,
+        time.minute,
+      );
+    });
+  }
+
+  Future<DateTime?> _selectDate() async {
+    int currentYear = DateTime.now().year;
+
+    DateTime? datePicked = await showDatePicker(
+      context: context,
+      initialDate: _date,
+      firstDate: new DateTime(currentYear - 3),
+      lastDate: new DateTime(currentYear + 2),
+    );
+
+    return datePicked;
+  }
+
+  Future<TimeOfDay?> _selectTime() async {
+    DateTime date = new DateTime.now();
+
+    TimeOfDay? timePicked = await showTimePicker(
+      context: context,
+      initialTime: TimeOfDay(
+        hour: date.hour,
+        minute: date.minute,
+      ),
+    );
+
+    return timePicked;
+  }
+
+  String formatDate(DateTime date) {
+    return DateFormat('dd/MMM/y h:mm a').format(date);
   }
 }
